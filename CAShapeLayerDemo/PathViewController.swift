24eca98eae568f8489f208526e95bcea96afa943
Guanshan Liu <guanshan.liu@gmail.com>
2015-02-27 14:45:32 +0800

    path animation

--- CAShapeLayerDemo/PathViewController.swift
+++ CAShapeLayerDemo/PathViewController.swift
@@ -23,7 +23,30 @@ class PathViewController: TapToCloseViewController {
         view.layer.addSublayer(rectShape)
 
         // Apply effects here
-        
+
+        // fill with yellow
+        rectShape.fillColor = UIColor.yellowColor().CGColor
+
+        // 1
+        // begin with a circle with a 50 points radius
+        let startShape = UIBezierPath(roundedRect: bounds, cornerRadius: 50).CGPath
+        // animation end with a large circle with 500 points radius
+        let endShape = UIBezierPath(roundedRect: CGRect(x: -450, y: -450, width: 1000, height: 1000), cornerRadius: 500).CGPath
+
+        // set initial shape
+        rectShape.path = startShape
+
+        // 2
+        // animate the `path`
+        let animation = CABasicAnimation(keyPath: "path")
+        animation.toValue = endShape
+        animation.duration = 1 // duration is 1 sec
+        // 3
+        animation.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseOut) // animation curve is Ease Out
+        animation.fillMode = kCAFillModeBoth // keep to value after finishing
+        animation.removedOnCompletion = false // don't remove after finishing
+        // 4
+        rectShape.addAnimation(animation, forKey: animation.keyPath)
     }
 
 }
