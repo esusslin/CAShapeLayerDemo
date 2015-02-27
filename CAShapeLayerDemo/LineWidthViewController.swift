1eec6eef43f0b97755b887a9170bee1088852ac0
Guanshan Liu <guanshan.liu@gmail.com>
2015-02-27 13:32:15 +0800

    starter

---
+++ CAShapeLayerDemo/LineWidthViewController.swift
@@ -0,0 +1,35 @@
+//
+//  LineWidthViewController.swift
+//  CAShapeLayerDemo
+//
+//  Created by Guanshan Liu on 27/2/15.
+//  Copyright (c) 2015 Guanshan Liu. All rights reserved.
+//
+
+import UIKit
+
+class LineWidthViewController: TapToCloseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- CAShapeLayerDemo/MasterViewController.swift
+++ CAShapeLayerDemo/MasterViewController.swift
@@ -10,7 +10,14 @@ import UIKit
 
 class MasterViewController: UITableViewController {
 
-    var objects = [AnyObject]()
+    typealias ControllerInfo = (name: String, controller: Void -> UIViewController)
+
+    let infos: [ControllerInfo] = [
+        ("Rounded Corners", { return RoundedCornersViewController() } ),
+        ("path Animation", { return PathViewController() }),
+        ("lineWidth Animation", { return LineWidthViewController() }),
+        ("Stroke Animation", { return StrokeViewController() })
+    ]
 
 
     override func awakeFromNib() {
@@ -19,33 +26,11 @@ class MasterViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        self.navigationItem.leftBarButtonItem = self.editButtonItem()
-
-        let addButton = UIBarButtonItem(barButtonSystemItem: .Add, target: self, action: "insertNewObject:")
-        self.navigationItem.rightBarButtonItem = addButton
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
-    }
-
-    func insertNewObject(sender: AnyObject) {
-        objects.insert(NSDate(), atIndex: 0)
-        let indexPath = NSIndexPath(forRow: 0, inSection: 0)
-        self.tableView.insertRowsAtIndexPaths([indexPath], withRowAnimation: .Automatic)
-    }
-
-    // MARK: - Segues
-
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        if segue.identifier == "showDetail" {
-            if let indexPath = self.tableView.indexPathForSelectedRow() {
-                let object = objects[indexPath.row] as! NSDate
-            (segue.destinationViewController as! DetailViewController).detailItem = object
-            }
-        }
     }
 
     // MARK: - Table View
@@ -55,14 +40,12 @@ class MasterViewController: UITableViewController {
     }
 
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return objects.count
+        return infos.count
     }
 
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath) as! UITableViewCell
-
-        let object = objects[indexPath.row] as! NSDate
-        cell.textLabel!.text = object.description
+        cell.textLabel!.text = infos[indexPath.row].name
         return cell
     }
 
@@ -71,15 +54,9 @@ class MasterViewController: UITableViewController {
         return true
     }
 
-    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-        if editingStyle == .Delete {
-            objects.removeAtIndex(indexPath.row)
-            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-        } else if editingStyle == .Insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view.
-        }
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        presentViewController(infos[indexPath.row].controller(), animated: true, completion: nil)
     }
-
 
 }
 
